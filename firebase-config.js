bcdec7d6109676b267f6a5d5f7fd73756a96e245
nakagi1016 <tyuraratyura@gmail.com>
2025-06-10 23:53:30 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@ const firebaseConfig = {
   authDomain: "katenizer-85e4a.firebaseapp.com",
   databaseURL: "https://katenizer-85e4a-default-rtdb.firebaseio.com",
   projectId: "katenizer-85e4a",
-  storageBucket: "katenizer-85e4a.firebasestorage.app",
+  storageBucket: "katenizer-85e4a.appspot.com", // ←ここの修正も反映済み！
   messagingSenderId: "525326072527",
   appId: "1:525326072527:web:14c9ab2952956e46fd15ba"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// 互換モードで初期化（これが重要！）
+firebase.initializeApp(firebaseConfig);
