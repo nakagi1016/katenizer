1431992ee9a9fb69c9abd685938b2c5ca2865469
nakagi1016 <tyuraratyura@gmail.com>
2025-06-10 23:36:05 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDl4gMuF9e2Esst0FHOVOC3JhIQEFBYDBY",
+  authDomain: "katenizer-85e4a.firebaseapp.com",
+  databaseURL: "https://katenizer-85e4a-default-rtdb.firebaseio.com",
+  projectId: "katenizer-85e4a",
+  storageBucket: "katenizer-85e4a.firebasestorage.app",
+  messagingSenderId: "525326072527",
+  appId: "1:525326072527:web:14c9ab2952956e46fd15ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
